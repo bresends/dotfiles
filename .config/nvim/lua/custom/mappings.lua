e7edaeda9b8024cdff852f165a69c08504edaa62
Bruno <bruno.resendesantos45@gmail.com>
2024-02-14 20:42:22 -0300

    feat: added go addons

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -12,6 +12,20 @@ M.crates = {
   }
 }
 
+M.gopher = {
+  plugin = true,
+  n = {
+    ["<leader>gsj"] = {
+      "<cmd> GoTagAdd json <CR>",
+      "Add json struct tags"
+    },
+    ["<leader>gsy"] = {
+      "<cmd> GoTagAdd yaml <CR>",
+      "Add yaml struct tags"
+    }
+  }
+}
+
 M.general = {
   n = {
     ["<leader>pv"] = { "<cmd> bd <CR>", "close file" },
--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -114,7 +114,18 @@ local plugins = {
         color_square_width = 2,
       })
     end
-  }
+  },
+    {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+      require("core.utils").load_mappings("gopher")
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
 }
 
 return plugins
