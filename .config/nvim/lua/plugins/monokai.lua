727bfd95291ed87adace2cbce69cf2db6b9d658a
Bruno <bruno.resendesantos45@gmail.com>
2024-03-16 19:26:04 -0300

    feat: modularize the configs

---
+++ .config/nvim/lua/plugins/monokai.lua
@@ -0,0 +1,10 @@
+return {
+  "loctvl842/monokai-pro.nvim",
+  lazy = false,
+  config = function()
+    require("monokai-pro").setup()
+    vim.cmd.colorscheme "monokai-pro"
+  end,
+  priority = 1000
+}
+
