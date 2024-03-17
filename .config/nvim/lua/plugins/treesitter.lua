fd9ca23dc3e23a08f59ce834190639acfa8ca23f
Bruno <bruno.resendesantos45@gmail.com>
2024-03-17 09:24:42 -0300

    feat: add completions

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,13 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = { "lua" },
-      highlight = { enable = true, use_languagetree = true },
-      indent = { enable = true },  
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = { "lua" },
+			auto_install = true,
+			highlight = { enable = true, use_languagetree = true },
+			indent = { enable = true },
+		})
+	end,
 }
