c4bc3e550d9764e4ea3378a901367eaa3366d95e
Bruno Resende dos Santos <bruno.resendesantos45@gmail.com>
2024-12-10 20:45:12 -0300

    chore: update packages and remove search highlight

--- .config/nvim/lua/config/vim-options.lua
+++ .config/nvim/lua/config/vim-options.lua
@@ -6,6 +6,10 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
+-- No highlight after search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
 vim.opt.smartindent = true
 
 vim.opt.swapfile = false
