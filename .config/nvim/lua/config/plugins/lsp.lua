97b700ece3c8316d4cc04b2672104966c788af3f
Bruno Resende dos Santos <bruno.resendesantos45@gmail.com>
2024-12-13 20:42:53 -0300

    feat: add lua vim support to lsp

--- .config/nvim/lua/config/plugins/lsp.lua
+++ .config/nvim/lua/config/plugins/lsp.lua
@@ -5,12 +5,23 @@ return {
   },
   {
     "williamboman/mason-lspconfig.nvim",
+    dependencies = {"williamboman/mason.nvim"},
     opts = {
       ensure_installed = { "lua_ls"},
     },
   },
   {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
+    dependencies = {"folke/lazydev.nvim"},
     config = function()
       require("lspconfig").lua_ls.setup {}
     end,
