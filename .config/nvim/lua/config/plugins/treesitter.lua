07ec35f5f9cb2062cb74b93e7fc12eca49aef1bf
Bruno Resende dos Santos <bruno.resendesantos45@gmail.com>
2024-12-14 20:13:27 -0300

    fix(treesitter): add default languages for treesitter

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -4,6 +4,8 @@ return {
     build = ":TSUpdate",
     config = function()
       local config = require("nvim-treesitter.configs")
+      -- Nvim required parsers
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
       config.setup({
         auto_install = true,
