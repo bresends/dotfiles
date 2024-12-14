b600c6e521711403a77f76036f9ff5e8c5a7ef70
Bruno Resende dos Santos <bruno.resendesantos45@gmail.com>
2024-12-14 19:46:07 -0300

    feat(nvim): add new default vim options

--- .config/nvim/lua/config/vim-options.lua
+++ .config/nvim/lua/config/vim-options.lua
@@ -1,26 +1,30 @@
+-- Line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+-- Indentation
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.smartindent = true
 
--- No highlight after search
-vim.opt.hlsearch = false
+-- Search
+vim.opt.hlsearch = false -- no highlight after search
 vim.opt.incsearch = true
+vim.opt.ignorecase = true -- search plugins with lowercase
 
 -- Splits
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
-vim.opt.smartindent = true
-
+-- Undo
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true -- Keep undo history from closed sessions
 
-vim.opt.termguicolors = true
+-- Visual block mode
+vim.opt.virtualedit = "block" -- Allow to select any region with Visual Block mode
 
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+-- Visuals
+vim.opt.termguicolors = true
