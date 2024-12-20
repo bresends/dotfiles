121e8ee62cd67a76b00eb84cf327b38e26c09284
Bruno Resende dos Santos <bruno.resendesantos45@gmail.com>
2024-12-20 16:51:44 -0300

    feat(treesitter): add keymaps to text objects

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -8,9 +8,22 @@ return {
       config.setup({
         -- Nvim required parsers
         ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+
         auto_install = true,
+
         highlight = { enable = true },
+
         indent = { enable = true },
+
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<Leader>ss", -- normal mode
+            node_incremental = "<Leader>ss", -- visual mode
+            node_decremental = "<Leader>sd", -- visual mode
+            scope_incremental = "<Leader>sc", -- visual mode
+          },
+        },
       })
     end
   }
