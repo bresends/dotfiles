61eabdd5f9cbb720472f32f3a9c9fe1d8c9af1a0
Bruno Resende dos Santos <bruno.resendesantos45@gmail.com>
2024-12-21 06:38:15 -0300

    feat(keymaps): move nvchad keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,23 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- Write file
+vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
+
+-- Escape with jk
+vim.keymap.set("i", "jk", "<ESC>", { nowait = true, desc = "Escape insert mode" })
+
+-- Move lines in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
+
 -- Lua Execution Shortcuts
-vim.keymap.set("n", "<leader><leader>x", "<cmd>source % | echo 'Sourced current file!'<CR>",
-  { desc = "Source the current file " })
+vim.keymap.set(
+	"n",
+	"<leader><leader>x",
+	"<cmd>source % | echo 'Sourced current file!'<CR>",
+	{ desc = "Source the current file " }
+)
 vim.keymap.set("v", "<leader><leader>x", ":lua<CR>", { desc = "Execute selected Lua code" })
 
 -- Shared Clipboard Shortcuts
@@ -13,21 +27,30 @@ vim.keymap.set({ "n", "x" }, "<leader>d", [["+d]], { desc = "Cut to system clipb
 vim.keymap.set("n", "<Leader>yf", "<cmd>%y+<CR>", { desc = "general copy whole file" })
 
 -- LSP
-vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = "LSP - Rename Symbol" })
-vim.keymap.set('n', '<leader>.', vim.lsp.buf.code_action, { desc = "LSP - Code actions" })
-vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, { desc = "LSP - Go to the definition" })
-vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, { desc = "LSP - Show all references " })
-vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, { desc = "LSP - Go to the implementation" })
-vim.keymap.set('n', '<leader>ds', vim.lsp.buf.document_symbol, { desc = "LSP - List all symbols in the current buffer" })
-vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help,
-  { desc = "LSP - Show signature help for the function being typed" })
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP - Rename Symbol" })
+vim.keymap.set("n", "<leader>.", vim.lsp.buf.code_action, { desc = "LSP - Code actions" })
+vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "LSP - Go to the definition" })
+vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "LSP - Show all references " })
+vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "LSP - Go to the implementation" })
+vim.keymap.set(
+	"n",
+	"<leader>ds",
+	vim.lsp.buf.document_symbol,
+	{ desc = "LSP - List all symbols in the current buffer" }
+)
+vim.keymap.set(
+	"i",
+	"<C-k>",
+	vim.lsp.buf.signature_help,
+	{ desc = "LSP - Show signature help for the function being typed" }
+)
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
