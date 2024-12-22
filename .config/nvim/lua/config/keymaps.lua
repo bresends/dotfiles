2c58636745217e750558745e5f725adf43c91035
Bruno Resende dos Santos <bruno.resendesantos45@gmail.com>
2024-12-22 10:51:59 -0300

    refactor: improve telescope config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,58 +1,54 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+local keymap = vim.keymap.set
 
 -- Write file
-vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
+keymap("n", "<leader>w", "<cmd>w<CR>")
 
 -- Escape with jk
-vim.keymap.set("i", "jk", "<ESC>", { nowait = true, desc = "Escape insert mode" })
+keymap("i", "jk", "<ESC>", { nowait = true, desc = "Escape insert mode" })
 
 -- Move lines in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
+keymap("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+keymap("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
 
--- Window navigation
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+-- Navigation
+keymap("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+keymap("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+keymap("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+keymap("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+keymap("n", "<C-u>", "<C-u>zz", { desc = "Center cursor after moving up half-page" })
+keymap("n", "<C-d>", "<C-d>zz", { desc = "Center cursor after moving down half-page" })
 
 -- Lua Execution Shortcuts
-vim.keymap.set(
+keymap(
 	"n",
 	"<leader><leader>x",
 	"<cmd>source % | echo 'Sourced current file!'<CR>",
 	{ desc = "Source the current file " }
 )
-vim.keymap.set("v", "<leader><leader>x", ":lua<CR>", { desc = "Execute selected Lua code" })
+keymap("v", "<leader><leader>x", ":lua<CR>", { desc = "Execute selected Lua code" })
 
 -- Shared Clipboard Shortcuts
-vim.keymap.set({ "n", "x" }, "<leader>y", [["+y]], { desc = "Copy to system clipboard" })
-vim.keymap.set({ "n", "x" }, "<leader>p", [["+P]], { desc = "Paste from the system clipboard" })
-vim.keymap.set({ "n", "x" }, "<leader>d", [["+d]], { desc = "Cut to system clipboard" })
-vim.keymap.set("n", "<Leader>yf", "<cmd>%y+<CR>", { desc = "general copy whole file" })
+keymap({ "n", "x" }, "<leader>y", [["+y]], { desc = "Copy to system clipboard" })
+keymap({ "n", "x" }, "<leader>p", [["+P]], { desc = "Paste from the system clipboard" })
+keymap({ "n", "x" }, "<leader>d", [["+d]], { desc = "Cut to system clipboard" })
+keymap("n", "<Leader>yf", "<cmd>%y+<CR>", { desc = "general copy whole file" })
 
 -- Lazy
-vim.keymap.set("n", "<Leader>lu", "<cmd>Lazy update<CR>", { desc = "Update Plugins" })
+keymap("n", "<Leader>lu", "<cmd>Lazy update<CR>", { desc = "Update Plugins" })
+
+-- Diagnostic keymaps
+keymap("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- LSP
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP - Rename Symbol" })
-vim.keymap.set("n", "<leader>.", vim.lsp.buf.code_action, { desc = "LSP - Code actions" })
-vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "LSP - Go to the definition" })
-vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "LSP - Show all references " })
-vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "LSP - Go to the implementation" })
-vim.keymap.set(
-	"n",
-	"<leader>ds",
-	vim.lsp.buf.document_symbol,
-	{ desc = "LSP - List all symbols in the current buffer" }
-)
-vim.keymap.set(
-	"i",
-	"<C-k>",
-	vim.lsp.buf.signature_help,
-	{ desc = "LSP - Show signature help for the function being typed" }
-)
+keymap("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP - Rename Symbol" })
+keymap("n", "<leader>.", vim.lsp.buf.code_action, { desc = "LSP - Code actions" })
+keymap("n", "<leader>gd", vim.lsp.buf.definition, { desc = "LSP - Go to the definition" })
+keymap("n", "<leader>gr", vim.lsp.buf.references, { desc = "LSP - Show all references " })
+keymap("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "LSP - Go to the implementation" })
+keymap("n", "<leader>ds", vim.lsp.buf.document_symbol, { desc = "LSP - List all symbols in the current buffer" })
+keymap("i", "<C-k>", vim.lsp.buf.signature_help, { desc = "LSP - Show signature help for the function being typed" })
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
--- .config/nvim/lua/config/plugins/telescope.lua
+++ .config/nvim/lua/config/plugins/telescope.lua
@@ -1,11 +1,53 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	event = "VimEnter",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			cond = function()
+				return vim.fn.executable("make") == 1
+			end,
+		},
+		{ "nvim-telescope/telescope-ui-select.nvim" },
+		{ "nvim-tree/nvim-web-devicons" },
+	},
 	config = function()
+		require("telescope").setup({
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown(),
+				},
+			},
+		})
+
+		-- Enable Telescope extensions if they are installed
+		pcall(require("telescope").load_extension, "fzf")
+		pcall(require("telescope").load_extension, "ui-select")
+
+		-- See `:help telescope.builtin`
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[S]earch [F]iles" })
+		vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "[S]earch Recent Files" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[ ] Find existing buffers" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+		vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+
+		-- Slightly advanced example of overriding default behavior and theme
+		vim.keymap.set("n", "<leader>/", function()
+			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+				winblend = 10,
+				previewer = false,
+			}))
+		end, { desc = "[/] Fuzzily search in current buffer" })
+
+		-- Shortcut for searching your Neovim configuration files
+		vim.keymap.set("n", "<leader>fn", function()
+			builtin.find_files({ cwd = vim.fn.stdpath("config") })
+		end, { desc = "[S]earch [N]eovim files" })
 	end,
 }
