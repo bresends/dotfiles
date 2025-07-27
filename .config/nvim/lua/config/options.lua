6839dc46a9f95c0095338d45bd07f4c7b8c3f1a7
Bruno Resende dos Santos <bruno.resendesantos45@gmail.com>
2025-07-27 17:43:41 -0300

    refactor: move options to separate file

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,72 @@
+-- Basic settings
+vim.opt.number = true                              -- Line numbers
+vim.opt.relativenumber = true                      -- Relative line numbers
+vim.opt.cursorline = true                          -- Highlight current line
+vim.opt.wrap = false                               -- Don't wrap lines
+vim.opt.scrolloff = 10                             -- Keep 10 lines above/below cursor
+vim.opt.sidescrolloff = 8                          -- Keep 8 columns left/right of cursor
+
+-- Indentation
+vim.opt.tabstop = 2                                -- Tab width
+vim.opt.shiftwidth = 2                             -- Indent width
+vim.opt.softtabstop = 2                            -- Soft tab stop
+vim.opt.expandtab = true                           -- Use spaces instead of tabs
+vim.opt.smartindent = true                         -- Smart auto-indenting
+vim.opt.autoindent = true                          -- Copy indent from current line
+
+-- Clipboard
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- Search settings
+vim.opt.ignorecase = true                          -- Case insensitive search
+vim.opt.smartcase = true                           -- Case sensitive if uppercase in search
+vim.opt.hlsearch = false                           -- Don't highlight search results
+vim.opt.incsearch = true                           -- Show matches as you type
+vim.opt.inccommand = 'split'                       -- Show substitutions live
+
+-- Visual settings
+vim.opt.termguicolors = true                       -- Enable 24-bit colors
+vim.o.showmode = false                             -- Mode will be shown in the status line
+vim.opt.signcolumn = 'yes'                         -- Always show sign column
+vim.opt.matchtime = 2                              -- How long to show matching bracket
+vim.opt.cmdheight = 1                              -- Command line height
+vim.opt.completeopt = 'menuone,noinsert,noselect'  -- Completion options
+vim.opt.showmode = false                           -- Don't show mode in command line
+vim.opt.pumheight = 10                             -- Popup menu height
+vim.opt.pumblend = 10                              -- Popup menu transparency
+vim.opt.winblend = 0                               -- Floating window transparency
+vim.opt.conceallevel = 0                           -- Don't hide markup
+vim.opt.concealcursor = ''                         -- Don't hide cursor line markup
+vim.opt.lazyredraw = true                          -- Don't redraw during macros
+vim.opt.synmaxcol = 300                            -- Syntax highlighting limit
+
+-- File handling
+vim.opt.backup = false                             -- Don't create backup files
+vim.opt.writebackup = false                        -- Don't create backup before writing
+vim.opt.swapfile = false                           -- Don't create swap files
+vim.opt.undofile = false                           -- Persistent undo
+vim.opt.updatetime = 250                           -- Faster completion
+vim.opt.autoread = true                            -- Auto reload files changed outside vim
+vim.opt.autowrite = false                          -- Don't auto save
+vim.opt.timeoutlen = 300                           -- Decrease mapped sequence wai time (Esc)
+
+-- Folding settings
+vim.opt.foldmethod = 'expr'                             -- Use expression for folding
+vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'    -- Use treesitter for folding
+vim.opt.foldlevel = 99                                  -- Start with all folds open
+
+-- Behavior settings
+vim.opt.hidden = true                              -- Allow hidden buffers
+vim.opt.modifiable = true                          -- Allow buffer modifications
+vim.opt.backspace = 'indent,eol,start'             -- Better backspace behavior
+vim.opt.autochdir = false                          -- Don't auto change directory
+vim.opt.selection = 'exclusive'                    -- Selection behavior
+vim.opt.mouse = 'a'                                -- Enable mouse support
+
+-- Split behavior
+vim.opt.splitbelow = true                          -- Horizontal splits go below
+vim.opt.splitright = true                          -- Vertical splits go right
+
