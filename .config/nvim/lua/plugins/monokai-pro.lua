52ec8d338c30d0c71443daa7831ffa2df98c353c
Bruno Resende dos Santos <bruno.resendesantos45@gmail.com>
2025-07-28 06:39:51 -0300

    feat: add styling for markdown

--- .config/nvim/lua/plugins/monokai-pro.lua
+++ .config/nvim/lua/plugins/monokai-pro.lua
@@ -3,7 +3,14 @@ return {
     "loctvl842/monokai-pro.nvim",
     priority = 1000,
     config = function()
-      require("monokai-pro").setup()
+      require("monokai-pro").setup({
+        override = function(colors)
+          return {
+            ["@markup.list.checked.markdown"] = { fg = colors.base.red, italic = false },
+          }
+        end
+
+      })
       vim.cmd("colorscheme monokai-pro")
     end
   }
