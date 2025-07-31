cd39685b54a694a6536b041b25314cc2e59f65bf
Bruno Resende dos Santos <bruno.resendesantos45@gmail.com>
2025-07-31 19:00:31 -0300

    chore: remove comments from config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -218,12 +218,6 @@ return {
 
 			-- Ensure the servers and tools above are installed
 			--
-			-- To check the current status of installed tools and/or manually install
-			-- other tools, you can run
-			--    :Mason
-			--
-			-- You can press `g?` for help in this menu.
-			--
 			-- `mason` had to be setup earlier: to configure its options see the
 			-- `dependencies` table for `nvim-lspconfig` above.
 			--
