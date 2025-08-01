f07107d67adae67d778c971dbbd82af227a6b0cc
Bruno Resende dos Santos <bruno.resendesantos45@gmail.com>
2025-07-31 22:13:05 -0300

    feat: add more styling to markdown files

--- .config/nvim/lua/plugins/monokai-pro.lua
+++ .config/nvim/lua/plugins/monokai-pro.lua
@@ -1,17 +1,20 @@
 return {
-  {
-    "loctvl842/monokai-pro.nvim",
-    priority = 1000,
-    config = function()
-      require("monokai-pro").setup({
-        override = function(colors)
-          return {
-            ["@markup.list.checked.markdown"] = { fg = colors.base.red, italic = false },
-          }
-        end
-
-      })
-      vim.cmd("colorscheme monokai-pro")
-    end
-  }
+	{
+		"loctvl842/monokai-pro.nvim",
+		priority = 1000,
+		config = function()
+			require("monokai-pro").setup({
+				override = function(colors)
+					return {
+						["@markup.heading.1.markdown"] = { fg = "#fc9867", italic = false }, --H1
+						["@markup.heading.2.markdown"] = { fg = "#fc9867", italic = false }, --H2
+						["@markup.heading.3.markdown"] = { fg = "#fc9867", italic = false }, --H3
+						["@markup.strong.markdown_inline"] = { fg = colors.base.yellow, italic = false }, --Bold
+						["@markup.list.checked.markdown"] = { fg = colors.base.red, italic = false }, --Checked
+					}
+				end,
+			})
+			vim.cmd("colorscheme monokai-pro")
+		end,
+	},
 }
