d11c0b093bb4d441cd8bfe189ecc88275e4c758e
Bruno Resende dos Santos <bruno.resendesantos45@gmail.com>
2025-08-26 21:34:22 -0300

    refactor(lsp): show underlines for all messages

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -143,7 +143,7 @@ return {
 			vim.diagnostic.config({
 				severity_sort = true,
 				float = { border = "rounded", source = "if_many" },
-				underline = { severity = vim.diagnostic.severity.ERROR },
+				underline = true,
 				signs = {
 					text = {
 						[vim.diagnostic.severity.ERROR] = "󰅚 ",
