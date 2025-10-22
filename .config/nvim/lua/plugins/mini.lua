19a269082f077fcc38328bca768e31ba9f480cb2
Bruno Resende dos Santos <bruno.resendesantos45@gmail.com>
2025-10-22 13:36:00 -0300

    refactor(nvim): remove oil and use mini.files

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,5 +1,21 @@
 return { -- Collection of various small independent plugins/modules
 	"echasnovski/mini.nvim",
+	keys = {
+		{
+			"-",
+			function()
+				require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+			end,
+			desc = "Open mini.files (directory of current file)",
+		},
+		{
+			"_",
+			function()
+				require("mini.files").open(vim.uv.cwd(), true)
+			end,
+			desc = "Open mini.files (cwd)",
+		},
+	},
 	config = function()
 		-- Better Around/Inside textobjects
 		--
@@ -15,5 +31,15 @@ return { -- Collection of various small independent plugins/modules
 		-- - sd'   - [S]urround [D]elete [']quotes
 		-- - sr)'  - [S]urround [R]eplace [)] [']
 		require("mini.surround").setup()
+
+		require("mini.pairs").setup()
+
+		require("mini.files").setup({
+			windows = {
+				preview = true,
+				width_focus = 30,
+				width_preview = 60,
+			},
+		})
 	end,
 }
