6e4d72af276c51427ea1a08c7aa3c390af0529a4
Bruno Resende dos Santos <bruno.resendesantos45@gmail.com>
2025-11-08 12:02:01 -0300

    refactor(lsp): move to old kickstart lsp keymaps

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -35,8 +35,7 @@ return {
 					end
 
 					-- Rename the variable under your cursor.
-					--  Most Language Servers support renaming across files, etc.
-					map("<leader>dg", vim.diagnostic.open_float, "Open [D]ia[G]gnostics")
+					map("<leader>ld", vim.diagnostic.open_float, "Open [L]SP [D]iagnostics")
 
 					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 
@@ -45,33 +44,33 @@ return {
 					map("<leader>ca", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
 
 					-- Find references for the word under your cursor.
-					map("grr", require("fzf-lua").lsp_references, "[G]oto [R]eferences")
+					map("gr", require("fzf-lua").lsp_references, "[G]oto [R]eferences")
 
 					-- Jump to the implementation of the word under your cursor.
 					--  Useful when your language has ways of declaring types without an actual implementation.
-					map("gri", require("fzf-lua").lsp_implementations, "[G]oto [I]mplementation")
+					map("gI", require("fzf-lua").lsp_implementations, "[G]oto [I]mplementation")
 
 					-- Jump to the definition of the word under your cursor.
 					--  This is where a variable was first declared, or where a function is defined, etc.
 					--  To jump back, press <C-t>.
-					map("grd", require("fzf-lua").lsp_definitions, "[G]oto [D]efinition")
+					map("gd", require("fzf-lua").lsp_definitions, "[G]oto [D]efinition")
 
 					-- WARN: This is not Goto Definition, this is Goto Declaration.
 					--  For example, in C this would take you to the header.
-					map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
 					-- Fuzzy find all the symbols in your current document.
 					--  Symbols are things like variables, functions, types, etc.
-					map("gO", require("fzf-lua").lsp_document_symbols, "Open Document Symbols")
+					map("<leader>ds", require("fzf-lua").lsp_document_symbols, "[D]ocument [S]ymbols")
 
 					-- Fuzzy find all the symbols in your current workspace.
 					--  Similar to document symbols, except searches over your entire project.
-					map("gW", require("fzf-lua").lsp_live_workspace_symbols, "Open Workspace Symbols")
+					map("<leader>ws", require("fzf-lua").lsp_live_workspace_symbols, "Open [W]orkspace [S]ymbols")
 
 					-- Jump to the type of the word under your cursor.
 					--  Useful when you're not sure what type a variable is and you want to see
 					--  the definition of its *type*, not where it was *defined*.
-					map("grt", require("fzf-lua").lsp_typedefs, "[G]oto [T]ype Definition")
+					map("gtd", require("fzf-lua").lsp_typedefs, "[G]oto [T]ype [D]efinition")
 
 					-- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
 					---@param client vim.lsp.Client
